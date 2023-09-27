8abba007451757675a94fee2981349b88d4eef09
DYlan <gownerbeats@gmail.com>
2023-09-27 16:35:56 -0500

    fix style video

--- src/pages/landing/components/Video/Video.jsx
+++ src/pages/landing/components/Video/Video.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
 import ReactPlayer from 'react-player';
 
 const videoYT = "https://www.youtube.com/watch?v=rv35m-qCq9U";
 
 export default function Video() {
+
   return (
-    <div>
+    <div className='w-[70vw] h-[50vh] sm:h-[70vh]'>
         <ReactPlayer
-            className="w-[70vw] rounded-md shadow-slate-800 shadow-md m-4"
             url={videoYT}
             loop={true}
-            playing={true}
+            // playing={true}
             volume={1}
+            width="100%"
+            height="100%"
         ></ReactPlayer>
     </div>
   )
