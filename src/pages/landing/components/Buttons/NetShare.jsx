b4ec41579fad1bc6ddc394ca5c0c3f331dd22b36
DYlan <gownerbeats@gmail.com>
2023-10-09 23:24:07 -0500

    fix deploy 4

--- src/pages/landing/components/Buttons/NetShare.jsx
+++ src/pages/landing/components/Buttons/NetShare.jsx
@@ -1,20 +1,18 @@
-import Link from 'next/link'
 import React from 'react'
+import Link from 'next/link'
 
 export default function NetShare({href, src, alt}) {
   return (
-    <>
-      {/* <Link
-          href={href}
-          className='h-9 w-9 m-1 border p-[.4rem] rounded-xl'
-          target="_blank"
-          rel="noopener noreferrer"
-      >
-          <img
-              src={src}
-              alt={alt}
-          />
-      </Link> */}
-    </>
+    <Link
+        href={href}
+        className='h-9 w-9 m-1 border p-[.4rem] rounded-xl'
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        <img
+            src={src}
+            alt={alt}
+        />
+    </Link>
   )
 }
