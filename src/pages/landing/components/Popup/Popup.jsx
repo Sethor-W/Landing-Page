25ed717364cffc567ea52457225034f6f051f86a
DYlan <gownerbeats@gmail.com>
2023-10-09 23:09:32 -0500

    refact desing complete2

--- src/pages/landing/components/Popup/Popup.jsx
+++ src/pages/landing/components/Popup/Popup.jsx
@@ -6,7 +6,6 @@ export default function Popup({children, active, handleDesactive}) {
   return (
     <div
         className={`fixed top-0 left-0 right-0 z-50 ${active ? 'block' : 'hidden'} w-full p-4 md:px-28 xl:px-40 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%)] max-h-full bg-black/[.5]`}
-        id="authentication-modal"
     >
         {/* <!-- Modal content --> */}
         <div
