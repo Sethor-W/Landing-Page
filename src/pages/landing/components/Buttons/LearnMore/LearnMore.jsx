7b55d83e74e4ed7c144bada1752fffc4e49c088e
DYlan <gownerbeats@gmail.com>
2023-10-09 23:29:23 -0500

    fix deploy 6

--- src/pages/landing/components/Buttons/LearnMore/LearnMore.jsx
+++ src/pages/landing/components/Buttons/LearnMore/LearnMore.jsx
@@ -3,11 +3,13 @@ import Link from 'next/link'
 
 export default function LearnMore({text, href}) {
   return (
-    <Link
-        className='text-white text-lg font-bold  py-4 px-7 rounded-2xl'
-        href={href}
-    >
-        {text}
-    </Link>
+    <>
+      <Link
+          className='text-white text-lg font-bold  py-4 px-7 rounded-2xl'
+          href={href}
+      >
+          {text}
+      </Link>
+    </>
   )
 }
